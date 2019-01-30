0f2025c925ecfea9db3ccc79597bb4a3fce7f8b6
Gastonm1 <gaston.marques@gmail.com>
2019-01-30 17:41:53 -0600

    Overall update

--- Assets/JS/javascript.js
+++ Assets/JS/javascript.js
@@ -4,4 +4,26 @@ $(document).ready(function() {
     var selected = $(this);
     $("a").removeclass("active");
   });
+
+var $a = $('.a'),
+$b = $('.b'),
+$c = $('.c'),
+$d = $('.d'),
+$home = $('.home'),
+$Autobiography = $('#Autobiography');
+
+$a.click(function(){
+  $a.fadeIn();
+  $b.fadeOut();
+  $c.fadeOut();
+  $d.fadeOut();
 });
+
+$b.click(function(){
+  $b.fadeIn();
+  $a.fadeOut();
+});
+});
+
+
+
